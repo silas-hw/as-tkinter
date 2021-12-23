ee7b43a7e1e11057794d7208b51bdb2ccf2bd54a
Silas HW <silas.hayes.williams@gmail.com>
2021-12-23 18:38:52 +0000

    Allow for @electron/remote to be used

--- library-challenge/electron version/main.js
+++ library-challenge/electron version/main.js
@@ -1,16 +1,24 @@
-const {app, BrowserWindow } = require('electron')
+const {app, BrowserWindow } = require('electron');
+
+require('@electron/remote/main').initialize();
+//require("@electron/remote/main").enable(webContents)
 
 function makeWindow () {
     const win = new BrowserWindow({
         width: 400,
         height: 400,
         resizable: false,
-        preload:'./preload.js'
+        webPreferences: {
+            nodeIntegration: true,
+            contextIsolation: false,
+            enableRemoteModule: true,
+        }
     })
 
+    require("@electron/remote/main").enable(win.webContents)
     win.loadFile('index.html')
 }
 
 app.whenReady().then(() => {
-    makeWindow()
+    makeWindow();
 })
